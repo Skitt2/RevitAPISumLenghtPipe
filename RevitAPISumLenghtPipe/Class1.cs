be62efddb24d4c146675ecfd4d7865365cdef437
Skitt2 <provaid@yandex.ru>
2022-05-05 20:11:09 +0300

    Add project files.

---
+++ RevitAPISumLenghtPipe/Class1.cs
@@ -0,0 +1,40 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Plumbing;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitAPISumLenghtPipe
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Main : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            IList<Reference> selectedElementRefList = uidoc.Selection.PickObjects(ObjectType.Element, new PipeFilter(), "Выберите трубы");
+            var pipeList = new List<Pipe>(); 
+            double lengthParam = 0;
+
+            foreach (var selectedElement in selectedElementRefList)
+            {
+                Pipe oPipe = doc.GetElement(selectedElement) as Pipe;
+                pipeList.Add(oPipe);
+                lengthParam += oPipe.get_Parameter(BuiltInParameter.CURVE_ELEM_LENGTH).AsDouble();
+            }
+
+            double length = UnitUtils.ConvertFromInternalUnits(lengthParam, UnitTypeId.Meters);
+            TaskDialog.Show("Длина выбранных труб", $"Длина выбранных труб: {length} м.");
+
+            return Result.Succeeded;
+        }
+    }
+}
---
+++ RevitAPISumLenghtPipe/PipeFilter.cs
@@ -0,0 +1,24 @@
+﻿using Autodesk.Revit.UI.Selection;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Plumbing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitAPISumLenghtPipe
+{
+    class PipeFilter : ISelectionFilter
+    {
+        public bool AllowElement(Element elem)
+        {
+            return elem is Pipe;
+        }
+
+        public bool AllowReference(Reference reference, XYZ position)
+        {
+            return false;
+        }
+    }
+}
